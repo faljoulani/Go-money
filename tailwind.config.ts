5ce3eb92f26980981e09314be1b5532708cf0ff0
Rawzi Al Horman <rawzialhorman@Rawzis-MacBook-Pro.local>
2025-08-25 10:51:34 +0300

    feat: main navigation

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#010663',
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config;
+
